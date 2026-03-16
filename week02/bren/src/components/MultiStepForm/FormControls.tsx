be689bf1621244a268251a85ed580b9d70802c29
bsh-ko <ssuh3872@gmail.com>
2026-03-16 15:02:33 +0900

    style : 버튼에 마진 주기

--- week02/bren/src/components/MultiStepForm/FormControls.tsx
+++ week02/bren/src/components/MultiStepForm/FormControls.tsx
@@ -7,7 +7,7 @@ export const FormControls = () => {
       <button onClick={prevStep} disabled={state.step === 1} className="disabled:opacity-20">
         Back
       </button>
-      <button onClick={nextStep} className="bg-blue-500 text-white px-4 py-2 rounded">
+      <button onClick={nextStep} className="bg-blue-500 text-white px-4 py-2 mx-2 rounded">
         Next
       </button>
     </div>
